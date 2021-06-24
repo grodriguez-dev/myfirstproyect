6532de7ae0d72590429cfc64a3442590ef90e72f
grodriguez-dev <grodriguez.dev99@gmail.com>
2021-06-23 23:14:12 -0400

    first commit

---
+++ pages/about.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h1>About Us</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate tempore temporibus aliquam maxime, corporis iste aperiam laborum, earum mollitia perferendis inventore laudantium, voluptas necessitatibus fuga officiis quos maiores reiciendis facilis!</p>
+    </div>
+</template>
+
+<script>
+export default {
+    head() {
+    return {
+      title: 'Posts | Nuxt application',
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: "website about posts"
+        }
+      ],
+      htmlAttrs: {
+        lang: 'en'
+      }
+    }
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1>Welcome to my first nuxt app</h1>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: 'Application Nuxt Posts | home',
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: "website about posts"
+        }
+      ],
+      htmlAttrs: {
+        lang: 'en'
+      }
+    }
+  }
+}
+</script>
